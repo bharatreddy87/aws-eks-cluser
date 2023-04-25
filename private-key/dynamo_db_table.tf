155c420ecc35b56a8aca2eda7027e849efd1de46
bharatreddy87 <121635679+bharatreddy87@users.noreply.github.com>
2023-04-25 12:25:22 -0500

    Update dynamo_db_table.tf

--- private-key/dynamo_db_table.tf
+++ private-key/dynamo_db_table.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks1"
+  name           = "terraform-locks111"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
