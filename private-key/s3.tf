b9660caf14af72ab871d4bae56636c5c3f3120d6
bharatreddy87 <121635679+bharatreddy87@users.noreply.github.com>
2023-04-27 18:45:04 -0500

    Update s3.tf

--- private-key/s3.tf
+++ private-key/s3.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "AKIAQMZRBKZATX5H3Z4G"
-  secret_key = "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"
+  access_key = "AKIAZYBNKTFCPLNRUT55"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform-on-aws-eks-bsbpr28" {
-  bucket = "terraform-on-aws-eks-bsbpr2811"
+  bucket = "terraform-on-aws-eks-bsbpr28"
   versioning {
     enabled = true
   }
