40b17e5a473f92ca74d8e267a1e5fac1395aaef0
bharatreddy87 <121635679+bharatreddy87@users.noreply.github.com>
2023-04-27 23:40:41 -0500

    Update s3.tf

--- private-key/s3.tf
+++ private-key/s3.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region     = "us-east-1"
+  region     = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform-on-aws-eks-bsbpr28" {
-  bucket = "terraform-remote-state-file-bsbpr28"
+  bucket = "terraform-on-aws-eks-bsbpr28"
   versioning {
     enabled = true
   }
